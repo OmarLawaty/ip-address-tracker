f9b16e6940ccfada4424b2fec42e991a226132cc
Omar Mohamed <omarlawatey@gmail.com>
2022-07-04 21:30:33 +0200

    fix: Change http Api to https Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,48 +2,44 @@ import axios from 'axios';
 
 const GeoIPFiyApi = (setIPInfo, IPAddress, setIPAddress, setIsLoading, setApiErr) => {
   axios
-    .get(`http://ip-api.com/json/${IPAddress}`)
-    .then(
-      ({
-        data
-        // : { query: ip, isp, country, city, timezone, lat, lon }
-      }) => {
-        console.log(data);
+    .get(`https://ipwho.is/${IPAddress}`)
+    .then(({ data }) => {
+      console.log(IPAddress);
+      console.log(data);
 
-        if (data.status === 'success') {
-          // Set IP values
-          setIPInfo({
-            ip: data.query,
-            isp: data.isp,
-            locationInfo: {
-              location: `${data.country}, ${data.city}`,
-              UTCTimezone: `${data.timezone}`,
-              lat: data.lat,
-              lng: data.lon
-            }
-          });
-          setIPAddress(pervValue => (pervValue !== '' ? pervValue : data.query));
-        }
-
-        if (data.status === 'fail') {
-          // Set IP values
-          setIPInfo({
-            ip: IPAddress,
-            isp: 'unknown',
-            locationInfo: {
-              location: 'unknown',
-              UTCTimezone: 'unknown',
-              lat: 0,
-              lng: 0
-            }
-          });
-          setIPAddress(pervValue => (pervValue !== '' ? pervValue : data.query));
-        }
-
-        // Stop the loading component
-        setIsLoading(false);
+      if (data.success) {
+        // Set IP values
+        setIPInfo({
+          ip: data.ip,
+          isp: data.connection.isp,
+          locationInfo: {
+            location: `${data.country}, ${data.city}`,
+            UTCTimezone: `UTC ${data.timezone.utc}`,
+            lat: data.latitude,
+            lng: data.longitude
+          }
+        });
+        setIPAddress(pervValue => (pervValue !== '' ? pervValue : data.ip));
       }
-    )
+
+      if (!data.success) {
+        // Set IP values
+        setIPInfo({
+          ip: IPAddress,
+          isp: 'unknown',
+          locationInfo: {
+            location: 'unknown',
+            UTCTimezone: 'unknown',
+            lat: 0,
+            lng: 0
+          }
+        });
+        setIPAddress(pervValue => (pervValue !== '' ? pervValue : data.ip));
+      }
+
+      // Stop the loading component
+      setIsLoading(false);
+    })
     .catch(error => {
       // Activate err message
       if (error.response) setApiErr(error.response.data);
